83983c3638b0ee8edd6ea01fdb40fba879a986de
ElinVasiliy <ElinVasiliy@mail.ru>
2019-12-15 23:32:52 +0400

    Добавил движение каробля, фона. Сделал префабы. Сделал верстку кнопок управления.

---
+++ Assets/Scripts/MoveBackground.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour
+{
+    [Range(0, 3)]
+    public float speed = 1;
+    private Animator animator;
+
+    [Header("Camera")]
+    public Camera cam;
+    private float minX, maxX, minZ, maxZ;
+    private Vector2 centrCam; // Центр камеры.
+    private float sizeCam; // Ширина камеры.
+    private float sizePlaneX = 10; // Кф для растяжения по длинне Plane.
+    private float sizePlaneZ = 20; // Кф для растяжения по высоте Plane.
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        sizeCam = cam.orthographicSize * cam.aspect; // Получили половину ширины камеры.
+        print(sizeCam);
+        centrCam = cam.transform.position; // Получаем центр камеры, т.к. пивот в центре камеры.
+
+        minX = centrCam.x - sizeCam; // Левый край камеры.
+        maxX -= minX; // Правый край камеры.
+        minZ = centrCam.y - sizeCam; // Нижний край камеры.
+        maxZ -= minZ; // Верхний край камеры.
+
+        transform.localScale = new Vector3((maxX - minX) / sizePlaneX, transform.localScale.y, (maxZ - minZ) / sizePlaneZ);
+
+        //ScaleBackground();
+    }
+
+    private void Update()
+    {
+        animator.speed = speed;
+    }
+
+    private void ScaleBackground()
+    {
+        minX = centrCam.x - sizeCam; // Левый край камеры.
+        maxX -= minX; // Правый край камеры.
+        minZ = centrCam.y - sizeCam; // Нижний край камеры.
+        maxZ -= minZ; // Верхний край камеры.
+
+        transform.localScale = new Vector3((maxX - minX) / sizePlaneX, transform.localScale.y, (maxZ - minZ) / sizePlaneZ);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Player")]
+    public float horizontalSpeed;    
+    private float speed;
+
+    Shoot shoot;
+
+    [Header("Camera")]
+    public Camera cam;
+    private float minX, maxX;
+    private Vector2 centrCam; // Центр камеры.
+    private float widthCam; // Ширина камеры.
+
+    private void Start()
+    {
+        ScaleCamera();
+        shoot = GetComponent<Shoot>();
+    }
+
+    private void FixedUpdate()
+    {
+        transform.Translate(speed, 0, 0);
+
+        MoveToScreenBorder();
+    }
+
+    private void ScaleCamera()
+    {
+        widthCam = cam.orthographicSize * cam.aspect; // Получили половину ширины камеры.
+        centrCam = cam.transform.position; // Получаем центр камеры, т.к. пивот в центре камеры.
+
+        minX = centrCam.x - widthCam; // Левый край камеры.
+        maxX = centrCam.x + widthCam; // Правый край камеры.
+    }
+
+    public void LeftButtonMove()
+    {
+        speed = -horizontalSpeed;
+    }
+
+    public void RightButtonMove()
+    {
+        speed = horizontalSpeed;
+    }
+
+    public void StopMove()
+    {
+        speed = 0;
+    }
+
+    private void MoveToScreenBorder()
+    {
+        if(transform.position.x < minX)
+        {
+            transform.position = new Vector2(maxX, transform.position.y);
+        }
+        else if(transform.position.x > maxX)
+        {
+            transform.position = new Vector2(minX, transform.position.y);
+        }
+    }
+
+    private void ShootButton()
+    {
+        if(!GameObject.FindWithTag("Shoot"))
+        {
+            Instantiate(shoot);
+        }
+    }
+}
